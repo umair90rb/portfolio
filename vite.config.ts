17392bbb4dbb362a9c60f23d6805df969b02fbaf
umair <umair@getbee.com>
2025-07-28 12:15:41 +0500

    add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio',
   plugins: [react(), svgr(), tailwindcss()],
 });
